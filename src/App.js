508e34498f355a5dae9e949b1bade4d556ae5068
AniekutmfonGodwin <adriano8263@gmail.com>
2020-07-08 20:46:10 +0100

    start project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+
+import { 
+ } from './Components/Pages';
+
+
+function App() {
+  return (
+    <div>
+      <Switch>
+          <Route exact path="/">
+            
+            
+          </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+// import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#ff7300',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: '#e5c500',
+      // dark: will be calculated from palette.secondary.main,
+      // contrastText: '#ffcc00',
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    // contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    // tonalOffset: 0.2,
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+    <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
